4121543f80345498f2d0d5f6931685ecd2727325
Michael Young <mikeyoung13@gmail.com>
2012-05-05 11:49:06 -0400

    incorporate Twitter feed (rough cut)

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,3 +1,5 @@
+var twitterData = null;
+
 var categoryData = {
     animals: {
         name: "Animals",
@@ -133,6 +135,14 @@ function showCategory( urlObj, options )
     }
 }
 
+$(document).bind('pageinit',function() {
+        if (twitterData === null) {
+            alert("loading twitter data!");
+            getTwitter();
+        }
+    }
+);
+
 // Listen for any attempts to call changePage().
 $(document).bind( "pagebeforechange", function( e, data ) {
     // We only want to handle changePage() calls where the caller is
@@ -142,8 +152,9 @@ $(document).bind( "pagebeforechange", function( e, data ) {
         // want to handle URLs that request the data for a specific
         // category.
         var u = $.mobile.path.parseUrl( data.toPage ),
-            re = /^#category-item/;
-        if ( u.hash.search(re) !== -1 ) {
+            categoryPath = /^#category-item/,
+            twitterPath = /^#twitter/;
+        if ( u.hash.search(categoryPath) !== -1 ) {
             // We're being asked to display the items for a specific category.
             // Call our internal method that builds the content for the category
             // on the fly based on our in-memory category data structure.
@@ -152,7 +163,15 @@ $(document).bind( "pagebeforechange", function( e, data ) {
             // Make sure to tell changePage() we've handled this call so it doesn't
             // have to do anything.
             e.preventDefault()
+        } else if (u.hash.search(twitterPath) !== -1)  {
+            if (twitterData === null) {
+                getTwitter();
+            }
+            showTwitter( u, data.options );
+            e.preventDefault()
+
         }
+
     }
 });
 
@@ -183,11 +202,13 @@ function getTwitter() {
             //print("success");
             //print(Object.keys(json));
 
+            twitterData = json;
+
             for (var i=0; i<json.length; i++) {
                 var tweet = json[i];
-                print("**************************");
-                print("Date: "+ tweet.created_at);
-                print("Text: "+ tweet.text);
+                console.log("**************************");
+                console.log("Date: "+ tweet.created_at);
+                console.log("Text: "+ tweet.text);
 
             }
 
@@ -205,4 +226,63 @@ function getTwitter() {
             //print("request is complete ");
         }
     });
+}
+
+function showTwitter( urlObj, options )
+{
+
+    // The pages we use to display our content are already in
+    // the DOM. The id of the page we are going to write our
+    // content into is specified in the hash before the '?'.
+        pageSelector = urlObj.hash.replace( /\?.*$/, "" );
+
+    if ( twitterData ) {
+        // Get the page we are going to dump our content into.
+        var $page = $( pageSelector ),
+
+        // Get the header for the page.
+            $header = $page.children( ":jqmData(role=header)" ),
+
+        // Get the content area element for the page.
+            $content = $page.children( ":jqmData(role=content)" ),
+
+        // The markup we are going to inject into the content
+        // area of the page.
+            markup = "<p>RAMBO Twitter Feed</p><ul data-role='listview' data-inset='true'>";
+
+        // Generate a list item for each item in the category
+        // and add it to our markup.
+        for ( var i = 0; i < twitterData.length; i++ ) {
+            markup += "<li>" + twitterData[i].text + "</li>";
+        }
+        markup += "</ul>";
+
+        // Find the h1 element in our header and inject the name of
+        // the category into it.
+        $header.find( "h1" ).html( "RAMNO - Twitter" );
+
+        // Inject the category items markup into the content element.
+        $content.html( markup );
+
+        // Pages are lazily enhanced. We call page() on the page
+        // element to make sure it is always enhanced before we
+        // attempt to enhance the listview markup we just injected.
+        // Subsequent calls to page() are ignored since a page/widget
+        // can only be enhanced once.
+        $page.page();
+
+        // Enhance the listview we just injected.
+        $content.find( ":jqmData(role=listview)" ).listview();
+
+        // We don't want the data-url of the page we just modified
+        // to be the url that shows up in the browser's location field,
+        // so set the dataUrl option to the URL for the category
+        // we just loaded.
+        options.dataUrl = urlObj.href;
+
+        // Now call changePage() and tell it to switch to
+        // the page we just modified.
+        $.mobile.changePage( $page, options );
+    }
+
 }
