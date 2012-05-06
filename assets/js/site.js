9a21b093678634b12e07348c57194dc7ae280e64
Michael Young <mikeyoung13@gmail.com>
2012-05-06 11:35:56 -0400

    remove test code and example code

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,143 +1,9 @@
 var twitterData = null;
 var calendarData = null;
-var categoryData = {
-    animals: {
-        name: "Animals",
-        description: "All your favorites from aardvarks to zebras.",
-        items: [
-            {
-                name: "Pets"
-            },
-            {
-                name: "Farm Animals"
-            },
-            {
-                name: "Wild Animals"
-            }
-        ]
-    },
-    colors: {
-        name: "Colors",
-        description: "Fresh colors from the magic rainbow.",
-        items: [
-            {
-                name: "Blue"
-            },
-            {
-                name: "Green"
-            },
-            {
-                name: "Orange"
-            },
-            {
-                name: "Purple"
-            },
-            {
-                name: "Red"
-            },
-            {
-                name: "Yellow"
-            },
-            {
-                name: "Violet"
-            }
-        ]
-    },
-    vehicles: {
-        name: "Vehicles",
-        description: "Everything from cars to planes.",
-        items: [
-            {
-                name: "Cars"
-            },
-            {
-                name: "Planes"
-            },
-            {
-                name: "Construction"
-            }
-        ]
-    }
-};
 var twitterTemplate = null;
 var calendarTemplate = null;
 
 //twitterData = [{"created_at":"Sun May 6 14:48:28 +0000 2012","text":"Good MTB weather people!1"},{"created_at":"Sun May 6 10:30:28 +0000 2012","text":"Good MTB weather people!2"},{"created_at":"Sun May 6 07:30:28 +0000 2012","text":"Good MTB weather people!3"}];
-
-// Load the data for a specific category, based on
-// the URL passed in. Generate markup for the items in the
-// category, inject it into an embedded page, and then make
-// that page the current active page.
-function showCategory( urlObj, options )
-{
-    var categoryName = urlObj.hash.replace( /.*category=/, "" ),
-
-    // Get the object that represents the category we
-    // are interested in. Note, that at this point we could
-    // instead fire off an ajax request to fetch the data, but
-    // for the purposes of this sample, it's already in memory.
-        category = categoryData[ categoryName ],
-
-    // The pages we use to display our content are already in
-    // the DOM. The id of the page we are going to write our
-    // content into is specified in the hash before the '?'.
-        pageSelector = urlObj.hash.replace( /\?.*$/, "" );
-
-    if ( category ) {
-        // Get the page we are going to dump our content into.
-        var $page = $( pageSelector ),
-
-        // Get the header for the page.
-            $header = $page.children( ":jqmData(role=header)" ),
-
-        // Get the content area element for the page.
-            $content = $page.children( ":jqmData(role=content)" ),
-
-        // The markup we are going to inject into the content
-        // area of the page.
-            markup = "<p>" + category.description + "</p><ul data-role='listview' data-inset='true'>",
-
-        // The array of items for this category.
-            cItems = category.items,
-
-        // The number of items in the category.
-            numItems = cItems.length;
-
-        // Generate a list item for each item in the category
-        // and add it to our markup.
-        for ( var i = 0; i < numItems; i++ ) {
-            markup += "<li>" + cItems[i].name + "</li>";
-        }
-        markup += "</ul>";
-
-        // Find the h1 element in our header and inject the name of
-        // the category into it.
-        $header.find( "h1" ).html( category.name );
-
-        // Inject the category items markup into the content element.
-        $content.html( markup );
-
-        // Pages are lazily enhanced. We call page() on the page
-        // element to make sure it is always enhanced before we
-        // attempt to enhance the listview markup we just injected.
-        // Subsequent calls to page() are ignored since a page/widget
-        // can only be enhanced once.
-        $page.page();
-
-        // Enhance the listview we just injected.
-        $content.find( ":jqmData(role=listview)" ).listview();
-
-        // We don't want the data-url of the page we just modified
-        // to be the url that shows up in the browser's location field,
-        // so set the dataUrl option to the URL for the category
-        // we just loaded.
-        options.dataUrl = urlObj.href;
-
-        // Now call changePage() and tell it to switch to
-        // the page we just modified.
-        $.mobile.changePage( $page, options );
-    }
-}
 
 function getFormattedDate(date) {
     console.log("Date to string",date.toDateString());
@@ -230,10 +96,6 @@ $(document).ready(function() {
 
 });
 
-
-
-
-
 $(document).bind('pagechange',function(event,data) {
         console.log("pagechange event!!!");
         if (twitterData === null) {
@@ -286,26 +148,16 @@ $(document).bind( "pagebeforechange", function( e, data ) {
 function getTwitter() {
 
     $.ajax({
-        // the URL for the request
         url: 'https://api.twitter.com/1/statuses/user_timeline.json',
-
-        // the data to send
-        // (will be converted to a query string)
         data: {
             user_id: 263189907,
             screen_name: "rambomtb",
             count: 10,
             trim_user: 1
         },
-
-        // whether this is a POST or GET request
         type: 'GET',
-
-        // the type of data we expect back
         dataType: 'jsonp',
 
-        // code to run if the request succeeds;
-        // the response is passed to the function
         success: function(json) {
             //print("success");
             //print(Object.keys(json));
@@ -323,14 +175,10 @@ function getTwitter() {
 
         },
 
-        // code to run if the request fails;
-        // the raw request and status codes are
-        // passed to the function
         error: function(xhr, status) {
             alert('Sorry, there was a problem!');
         },
 
-        // code to run regardless of success or failure
         complete: function(xhr, status) {
             //print("request is complete ");
         }
@@ -342,11 +190,8 @@ function getGoogleCal() {
     var now = (new Date()).toISOString();
 
     $.ajax({
-        // the URL for the request
         url: 'https://www.googleapis.com/calendar/v3/calendars/96msjdsgp3tcs3jv8kegvd9rhc@group.calendar.google.com/events',
 
-        // the data to send
-        // (will be converted to a query string)
         data: {
             fields: "items(description, end, location, start, summary)",
             singleEvents: true,
@@ -363,14 +208,10 @@ function getGoogleCal() {
             console.log(JSON.stringify(json, null, '  '));
         },
 
-        // code to run if the request fails;
-        // the raw request and status codes are
-        // passed to the function
         error: function(xhr, status) {
             alert('Sorry, there was a problem!');
         },
 
-        // code to run regardless of success or failure
         complete: function(xhr, status) {
             console.log("request is complete ");
         }
@@ -407,7 +248,7 @@ function processJQMListView($page, $content, options, urlObj) {
 
 function showTwitter( urlObj, options )
 {
-    pageSelector = getPageSelectorFromURL(urlObj);
+    var pageSelector = getPageSelectorFromURL(urlObj);
 
 
         var $page = $( pageSelector ),
@@ -423,7 +264,7 @@ function showTwitter( urlObj, options )
 
 function showCalendar( urlObj, options )
 {
-    pageSelector = getPageSelectorFromURL(urlObj);
+    var pageSelector = getPageSelectorFromURL(urlObj);
 
 
     var $page = $( pageSelector ),
