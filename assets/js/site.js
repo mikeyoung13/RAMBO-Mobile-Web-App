fa4388bfba9194aa956db480699066c510a1f7c9
Michael Young <mikeyoung13@gmail.com>
2012-07-03 10:19:35 -0400

    change "form submit" to regular <A href> so that iOS would open whitelisted Google Groups in browser within PhoneGap

--- assets/js/site.js
+++ assets/js/site.js
@@ -124,6 +124,24 @@ $(document).ready(function() {
         return false;
     });
 
+    //  Google Groups
+    $('#subscribe').bind("click", function(e) {
+        e.stopPropagation();
+        var email = $('#emailAddress').val().trim();
+        if (email.length === 0) {
+            alert("Please enter an email address.");
+            e.preventDefault();
+            return false;
+        }
+        if (confirm("Your browser will now be opened and your email will passed to Google Groups.  Please follow confirmation instructions given by Google.")) {
+            $(this).attr('href','http://groups.google.com/group/rambo-announce/boxsubscribe?email='+encodeURIComponent(email));
+            return true;
+        } else {
+            e.preventDefault();
+            return false;
+        }
+    });
+
     // compile templates
     twitterTemplate = Handlebars.compile($("#twitter-result-template").html());
     calendarTemplate = Handlebars.compile($("#calendar-result-template").html());
